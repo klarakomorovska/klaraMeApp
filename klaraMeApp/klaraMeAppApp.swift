c4dc75a11f32ef18af52aa60a46f776f6f9a24c7
Klara <klarka.komorovska@gmail.com>
2023-07-27 13:40:52 +0100

    Initial Commit

---
+++ klaraMeApp/klaraMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  klaraMeAppApp.swift
+//  klaraMeApp
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct klaraMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
